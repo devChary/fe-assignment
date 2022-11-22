074e3c11482c69a9f17e029e619f9e213d1ce318
devChary <jagannadhachary7182@gmail.com>
2022-11-22 23:09:41 +0530

    fix: webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,76 @@
-const path = require("path");
-const webpack = require("webpack");
-const { merge } = require("webpack-merge");
-const modeConfiguration = env => require(`./build-envs/webpack.${env}`)(env);
+const path = require('path');
 
-/* Plugins */
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = ({ mode } = { mode: "production" }) => {
-    console.log(`mode is: ${mode}`);
+let mode = 'development';
 
-    return merge({
-        mode,
-        entry: "./src/index.js",
-        devServer: {
-            hot: true,
-            open: true
-        },
-        output: {
-            publicPath: "/",
-            path: path.resolve(__dirname, "build"),
-            filename: "bundle.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    loader: "babel-loader"
-                },
-                {
-                    test: /\.(png|svg|j?g|gif|ico)$/,
-                    type: 'asset/resource',
-                    exclude: /node_modules/,
-                    use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
-                },
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                    type: 'asset/resource',
-                },
-            ]
-        },
+if (process.env.NODE_ENV === 'production') mode = 'production';
 
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: path.resolve( __dirname, 'public/index.html' ),
-                filename: 'index.html',
-                favicon: "public/favicon.ico",
-                manifest: "public/manifest.json"
-            }),
-            new webpack.HotModuleReplacementPlugin()
-        ],
-       
-        resolve: {
-            extensions: ['.js', '.jsx', '.json'],
-            modules: [path.resolve(__dirname, 'src'), 'node_modules']
-        },
-    
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    mode,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/',
     },
-        modeConfiguration(mode)
-    );
-};
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|j?g|svg|gif|ico)?$/,
+                type: 'asset/resource',
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                      limit: 8000,
+                      name: 'images/[hash]-[name].[ext]',
+                      publicPath: 'public/static/assets',
+                    }
+                  }, 'file-loader?name=[name].[ext]'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+
+    plugins :[
+        new MiniCssExtractPlugin(),
+        new HtmlWebPackPlugin({
+            template: path.resolve( __dirname, 'public/index.html' ),
+            filename: 'index.html',
+            favicon: "public/favicon.ico",
+            manifest: "public/manifest.json"
+        })
+    ],
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true,
+        static: {
+            directory: path.join(__dirname, "dist", "assets"),
+            publicPath: 'public/static/assets'
+        },
+    }
+}
