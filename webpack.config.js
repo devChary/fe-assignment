a73b23a4f2504bc1ac068404ea0c88e4c0428d42
devChary <jagannadhachary7182@gmail.com>
2022-11-27 15:28:56 +0530

    change build folder name

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     mode,
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js',
         publicPath: '/',
     },
@@ -69,7 +69,7 @@ module.exports = {
         hot: true,
         open: true,
         static: {
-            directory: path.join(__dirname, "dist", "assets"),
+            directory: path.join(__dirname, "build", "assets"),
             publicPath: 'src/assets'
         },
     }
